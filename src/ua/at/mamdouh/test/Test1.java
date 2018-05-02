a26c049ce930b08cb95eab5052a0ab22bcf4b651
IT-Mamdouh Adel <mamdouh-adel@mbcc.local>
2018-05-02 11:11:49 +0200

    third commit

--- src/ua/at/mamdouh/test/Test1.java
+++ src/ua/at/mamdouh/test/Test1.java
@@ -27,7 +27,7 @@ public class Test1 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hi My Test1 Servlet...");
+		response.getWriter().append("Hi My Test1 Servlet...Test1");
 	}
 
 	/**
